659181f5f1e1af7ca1f2684a4dcb4b0322f8199d
enunsnv <seoej0315@naver.com>
2024-12-10 17:55:52 +0900

    🔀merge: main

--- src/components/fishDetail/FishVideo.jsx
+++ src/components/fishDetail/FishVideo.jsx
@@ -2,17 +2,6 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background-color: #3c354a;
-  border-radius: 10px;
-  border: 1px solid #737373;
-  padding: 15px;
-  max-width: 800px;
-  margin: auto;
-  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
 `;
 
 const Title = styled.h3`
